b3c52db8cee293d636f58e24d9566da190a397f0
Joren S. Engbers <joren.engbers@gmail.com>
2026-04-16 13:42:20 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-white dark:bg-black">
-      <h1 className="text-4xl font-bold text-black dark:text-white">Hello World</h1>
+      <h1 className="text-4xl font-bold text-black dark:text-white">Hello Worldd</h1>
     </div>
   );
 }
